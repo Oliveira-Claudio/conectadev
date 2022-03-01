585b9c8e15babd3a314224b13f152d3fedf579f4
Claudio Oliveira <caon2006@gmail.com>
2022-03-01 06:59:31 -0300

    Initial code

---
+++ src/pages/Home/components/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="toolbar">
+        <div>
+          <span>Conecta Dev</span>
+        </div>
+        <div>
+          <button>Novo Post</button>
+          <span>Img1</span>
+          <span>Img2</span>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
